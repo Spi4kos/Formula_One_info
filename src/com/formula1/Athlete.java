62d302f332898df39b698f215a5af482e03081ef
Spi4kos <gordiev777@gmail.com>
2018-10-16 17:06:06 +0300

    0.21 geter and seter added

--- src/com/formula1/Athlete.java
+++ src/com/formula1/Athlete.java
@@ -1,18 +1,67 @@
 package com.formula1;
 
 public class Athlete {
-    int number;
-    String name;
-    String team;
-    int age;
-    String nation;
+    private int id;
+    private int number;
+    private String name;
+    private String team;
+    private int age;
+    private String nation;
     Athlete(int number, String name, String team, int age, String nation){
-        this.number = number;
-        this.name = name;
-        this.team = team;
-        this.age = age;
-        this.nation = nation;
+        setNumber(number);
+        setName(name);
+        setTeam(team);
+        setAge(age);
+        setNation(nation);
 
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 
     public String getSurname() {
--- src/com/formula1/Function.java
+++ src/com/formula1/Function.java
@@ -8,8 +8,8 @@ public final class Function {
     public static void search(String field, ArrayList <Athlete> list){
         for (int i = 0; i < list.size(); i++) {
             Athlete kent = list.get(i);
-            if(kent.name.toLowerCase().contains(field)){
-                System.out.println(kent.name + " (" + kent.team + "), " + kent.age + " years old, from: " + kent.nation );
+            if(kent.getName().toLowerCase().contains(field)){
+                System.out.println(kent.getName() + " (" + kent.getTeam() + "), " + kent.getAge() + " years old, from: " + kent.getNation() );
             }
 
         }
@@ -19,7 +19,7 @@ public final class Function {
         list.sort(Comparator.comparing(Athlete::getSurname));
         for (int i = 0; i < list.size(); i++){
             Athlete kent = list.get(i);
-            System.out.println(kent.name + " (" + kent.team + ")" );
+            System.out.println(kent.getName() + " (" + kent.getTeam() + ")" );
         }
     }
     public static void team(String field, ArrayList <Athlete> list) {
@@ -27,9 +27,9 @@ public final class Function {
         for (int i = 0; i < list.size(); i++) {
             Athlete kent = list.get(i);
 
-            if (kent.team.toLowerCase().contains(field)){
-                if (o < 1) {System.out.println(kent.team +": "); o=1;}
-                System.out.println(kent.name);
+            if (kent.getTeam().toLowerCase().contains(field)){
+                if (o < 1) {System.out.println(kent.getTeam() +": "); o=1;}
+                System.out.println(kent.getName());
 
             }
         }
